b46885bd5adc360de6fd86083e912fcbf341a040
yamiew511 <yamiew511@gmail.com>
2023-03-17 17:55:53 +0800

    - fix name AddMongoContext - fix the lifecycle-not-working bug

--- MongoGogo/MongoContextExtensions.cs
+++ MongoGogo/MongoContextExtensions.cs
@@ -6,7 +6,7 @@ namespace MongoGogo
 {
     public static class ServiceCollectionExtension
     {
-        public static IServiceCollection AddMongoContext2<TContext>(this IServiceCollection serviceCollection,
+        public static IServiceCollection AddMongoContext<TContext>(this IServiceCollection serviceCollection,
                                                             TContext mongoContext,
                                                             LifeCycleOption option = default)
             where TContext : class, IGoContext<TContext>
@@ -14,28 +14,22 @@ namespace MongoGogo
             //build the composite root of Context
             GoContainer goContainer = new GoContainer().Build(builder => builder.AddMongoContext(mongoContext, option));
 
-            //todo: lifeoption not work
             foreach (var registration in goContainer.Registrations)
             {
-                if (registration.Instance != null)
+                if(registration.LifeTime == LifeCycleType.Singleton)
                 {
-                    //singleton
                     serviceCollection.AddSingleton(serviceType: registration.RegisteredType,
-                                                   implementationInstance: registration.Instance);
+                                                   implementationInstance: goContainer.Resolve(registration.RegisteredType));
                 }
-                else
+                else if(registration.LifeTime == LifeCycleType.Scoped)
                 {
-                    var lifeTime = registration.LifeTime;
-                    if (lifeTime == LifeCycleType.Transient)
-                    {
-                        serviceCollection.AddTransient(serviceType: registration.RegisteredType,
+                    serviceCollection.AddScoped(serviceType: registration.RegisteredType,
+                                                implementationType: registration.MappedType);
+                }
+                else if(registration.LifeTime == LifeCycleType.Transient)
+                {
+                    serviceCollection.AddTransient(serviceType: registration.RegisteredType,
                                                        implementationType: registration.MappedType);
-                    }
-                    else
-                    {
-                        serviceCollection.AddScoped(serviceType: registration.RegisteredType,
-                                                    implementationType: registration.MappedType);
-                    }
                 }
             }
             return serviceCollection;
