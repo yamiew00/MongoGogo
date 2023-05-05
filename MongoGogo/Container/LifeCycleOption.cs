79ce3c0e60ad805327730db8e90b4f80eebd72fb
yamiew511 <yamiew511@gmail.com>
2023-05-05 15:01:17 +0800

    Set default lifecycle as Singleton.

--- MongoGogo/Container/LifeCycleOption.cs
+++ MongoGogo/Container/LifeCycleOption.cs
@@ -5,22 +5,22 @@
         /// <summary>
         /// Lifecycle of context in .net core. Scoped is default.
         /// </summary>
-        public LifeCycleType ContextLifeCycle { get; set; } = LifeCycleType.Scoped;
+        public LifeCycleType ContextLifeCycle { get; set; } = LifeCycleType.Singleton;
 
         /// <summary>
         /// Lifecycle of database in .net core. Scoped is default.
         /// </summary>
-        public LifeCycleType DatabaseLifeCycle { get; set; } = LifeCycleType.Scoped;
+        public LifeCycleType DatabaseLifeCycle { get; set; } = LifeCycleType.Singleton;
 
         /// <summary>
         /// Lifecycle of collection in .net core. Scoped is default.
         /// </summary>
-        public LifeCycleType MongoCollectionLifeCycle { get; set; } = LifeCycleType.Scoped;
+        public LifeCycleType MongoCollectionLifeCycle { get; set; } = LifeCycleType.Singleton;
 
         /// <summary>
         /// Lifecycle of collection in .net core. Scoped is default.
         /// </summary>
-        public LifeCycleType GoCollectionLifeCycle { get; set; } = LifeCycleType.Scoped;
+        public LifeCycleType GoCollectionLifeCycle { get; set; } = LifeCycleType.Singleton;
     }
 
     public enum LifeCycleType
