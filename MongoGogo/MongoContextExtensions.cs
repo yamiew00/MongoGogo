9953612281315572709fa13857cc013924972520
yamiew511 <yamiew511@gmail.com>
2023-05-19 15:34:54 +0800

    3.2.1 - fix a bug of GoObserver - decouple from `Microsoft.Extensions.DependencyInjection.Abstractions`

--- MongoGogo/MongoContextExtensions.cs
+++ MongoGogo/MongoContextExtensions.cs
@@ -1,14 +1,14 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using MongoGogo;
 using MongoGogo.Connection;
 using MongoGogo.Container;
 
-namespace MongoGogo
+namespace Microsoft.Extensions.DependencyInjection
 {
     public static class ServiceCollectionExtension
     {
         public static IServiceCollection AddMongoContext<TContext>(this IServiceCollection serviceCollection,
-                                                            TContext mongoContext,
-                                                            LifeCycleOption option = default)
+                                                                   TContext mongoContext,
+                                                                   LifeCycleOption option = default)
             where TContext : class, IGoContext<TContext>
         {
             //build the composite root of Context
