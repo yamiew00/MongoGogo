d1c5a6a9113ae442778900066620dbaa80b51d2d
yamiew511 <yamiew511@gmail.com>
2023-05-19 15:28:03 +0800

    constraint: GoObserver can be used if the collectionType is a derived set of `GoDocument`

--- MongoGogo/Container/GoContainer.cs
+++ MongoGogo/Container/GoContainer.cs
@@ -199,15 +199,18 @@ namespace MongoGogo.Container
                                                      mappedType: goCollectionImplementType,
                                                      option.GoCollectionLifeCycle));
 
-                //(3) IGoCollectionObserver
-                var observerServiceType = typeof(IGoCollectionObserver<>).GetGenericTypeDefinition()
+                //(3) IGoCollectionObserver<TDocument> where TDocument : GoDocument
+                if (typeof(GoDocument).IsAssignableFrom(collectionType))
+                {
+                    var observerServiceType = typeof(IGoCollectionObserver<>).GetGenericTypeDefinition()
                                                                          .MakeGenericType(collectionType);
-                var observerImplementType = typeof(GoCollectionObserver<>).GetGenericTypeDefinition()
-                                                                          .MakeGenericType(collectionType);
+                    var observerImplementType = typeof(GoCollectionObserver<>).GetGenericTypeDefinition()
+                                                                              .MakeGenericType(collectionType);
 
-                registrations.Add(new GoRegistration(registeredType: observerServiceType,
-                                                     mappedType: observerImplementType,
-                                                     option.GoCollectionObserverLifeCycle));
+                    registrations.Add(new GoRegistration(registeredType: observerServiceType,
+                                                         mappedType: observerImplementType,
+                                                         option.GoCollectionObserverLifeCycle));
+                }
             }
 
             return registrations;
